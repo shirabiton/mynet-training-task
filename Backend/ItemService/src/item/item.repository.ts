4a6aefeeea093e8d226678b462e88b98dd6699af
shirabiton <shirabiton387@gmail.com>
2025-02-20 15:37:55 +0200

    Use lodash for getting object by id

--- Backend/ItemService/src/item/item.repository.ts
+++ Backend/ItemService/src/item/item.repository.ts
@@ -1,9 +1,10 @@
 import { Item } from "@Libs/types/DB/item.type";
+import { find } from "lodash/fp";
 import items from "../mocks/items.json";
 
 export const ItemRepository = {
   getAll: async (): Promise<Item[]> => items,
 
   getItemById: async (id: string): Promise<Item | null> =>
-    items.find((item: Item) => item._id === id) || null,
+    find({ _id: id }, items) || null,
 };
--- Backend/UserService/src/user/user.repository.ts
+++ Backend/UserService/src/user/user.repository.ts
@@ -1,12 +1,13 @@
 import { User } from "@Libs/types/DB/user.types";
+import { find } from "lodash/fp";
 import users from "../mocks/users.json";
 
 export const UserRepository = {
   getAll: async (): Promise<User[]> => users,
 
   getUserById: async (id: string): Promise<User | null> =>
-    users.find((user: User) => user._id === id) || null,
+    find({ _id: id }, users) || null,
 
   getUserByEmail: async (email: string): Promise<User | null> =>
-    users.find((user: User) => user.email === email) || null,
+    find({ email }, users) || null,
 };
