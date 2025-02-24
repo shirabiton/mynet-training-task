1a9ed083fd708753f4363f633c707865aa4bb23f
shirabiton <shirabiton387@gmail.com>
2025-02-24 14:18:33 +0200

    ♻️ ref (backend): add use of lodash matchesProperty to get object by id

--- Backend/ItemService/src/item/item.repository.ts
+++ Backend/ItemService/src/item/item.repository.ts
@@ -1,10 +1,10 @@
 import { Item } from "@Libs/types/DB/item.type";
-import { find } from "lodash/fp";
+import { find, matchesProperty } from "lodash/fp";
 import items from "../mocks/items.json";
 
 export const ItemRepository = {
   getAll: async (): Promise<Item[]> => items,
 
   getItemById: async (id: string): Promise<Item | null> =>
-    find({ _id: id }, items) || null,
+    find(matchesProperty("_id", id), items) || null,
 };
--- Backend/UserService/src/user/user.repository.ts
+++ Backend/UserService/src/user/user.repository.ts
@@ -1,13 +1,13 @@
 import { User } from "@Libs/types/DB/user.types";
-import { find } from "lodash/fp";
+import { find, matchesProperty } from "lodash/fp";
 import users from "../mocks/users.json";
 
 export const UserRepository = {
   getAll: async (): Promise<User[]> => users,
 
   getUserById: async (id: string): Promise<User | null> =>
-    find({ _id: id }, users) || null,
+    find(matchesProperty("_id", id), users) || null,
 
   getUserByEmail: async (email: string): Promise<User | null> =>
-    find({ email }, users) || null,
+    find(matchesProperty("email", email), users) || null,
 };
