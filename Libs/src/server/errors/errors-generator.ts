4ee574cc8c683fe0f8b7a9c617cbcdae50368ac5
shirabiton <shirabiton387@gmail.com>
2025-03-02 10:14:51 +0200

    ⚰️ Remove unnecessary code

--- Libs/src/server/errors/errors-generator.ts
+++ Libs/src/server/errors/errors-generator.ts
@@ -10,7 +10,3 @@ export const throwUnauthorizedError = throwError(HttpStatusCode.Unauthorized);
 export const throwNotFoundError = throwError(HttpStatusCode.NotFound);
 
 export const throwBadRequestError = throwError(HttpStatusCode.BadRequest);
-
-export const throwInternalServerError = throwError(
-  HttpStatusCode.InternalServerError
-);
